a00262eb8c3f141bef4b0177c8d0a7a64ed3308a
omowhunmmy <ibirinde_omowunmi@yahoo.com>
2023-09-26 08:41:47 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,8 +16,8 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
-size_t listint_len(const listint_t *h);
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 size_t print_listint_safe(const listint_t *head);
